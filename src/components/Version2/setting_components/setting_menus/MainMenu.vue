757c4f82875b728d766959a2fff5a2fd42e0775d
unknown <cho89@live.co.kr>
2024-07-05 07:42:01 +0900

    uninstall dnd libraries

--- src/components/Version2/setting_components/setting_menus/MainMenu.vue
+++ src/components/Version2/setting_components/setting_menus/MainMenu.vue
@@ -1,74 +1,13 @@
 <template>
-  <div :class="['simple-page', 'limited-height']">
-    <Container
-      @drop="onDrop"
-      drag-class="opacity-ghost"
-      drop-class="opacity-ghost-drop"
-      @drag-enter="onDragStart"
-      @drag-leave="onDragLeave"
-    >
-      <Draggable v-for="item in items" :key="item.id" @click="handleClick(item)">
-        <div class="draggable-item">
-          {{ item.data }}
-        </div>
-      </Draggable>
-    </Container>
-  </div>
-  <p style="color: red;">{{ isDragging }}</p>
+  ddd
 </template>
 
 <script>
-import { Container, Draggable } from "vue-dndrop";
-import { applyDrag } from "@/utils/helpers";
-
 export default {
-  name: "DragClass",
-  components: { Container, Draggable },
-  data() {
-    return {
-      items: [
-        { data: '청출어람'},
-        { data: '안하무인'},
-        { data: '우이독경'},
-        { data: '고진감래'},
-        { data: '마이동풍'},
-        { data: '풍전등화'},
-        { data: '이이제이'},
-      ],
-      test: false,
-      isDragging: false,
-    };
-  },
-  methods: {
-    onDrop(dropResult) {
-      this.items = applyDrag(this.items, dropResult);
-      this.isDragging = !this.isDragging;
 
-    },
-    onDragStart() {
-      this.isDragging = !this.isDragging;
-    },
-    onDragLeave() {
-
-    }
-  },
-};
+}
 </script>
 
-<style scoped>
-.limited-height {
-  height: 60vh;
-  overflow: auto;
-}
-.draggable-item {
-  padding: 20px;
-  user-select: none;
-  cursor: pointer;
-}
-.opacity-ghost {
-  opacity: 0.4;
-}
-.opacity-ghost-drop {
-  opacity: 0.1;
-}
+<style>
+
 </style>
